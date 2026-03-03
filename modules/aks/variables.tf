f4069d43d131295d27886ac2359962d21e0b5c3f
RAJ0318 <rajkumardasari77@gmail.com>
2026-03-03 16:48:53 +0530

    updated

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -11,11 +11,11 @@ variable "service_principal_name" {
 #   default = ".ssh/id_rsa.pub"
 # }
 
-# variable "client_id" {}
-# variable "client_secret" {
-#   type = string
-#   sensitive = true
-# }
+variable "client_id" {}
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
 
 variable "node_pool_name" {
   
